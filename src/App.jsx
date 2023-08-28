2ba513e66e093d96060d7f53770f9997aace7294
lolxd122323 <roccogioia144@gmail.com>
2023-08-28 15:00:01 -0300

    lol

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Contador De Likes</h1>
+      <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        ❤️ {count}
+        </button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
